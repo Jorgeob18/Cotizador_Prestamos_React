8f5a4b23ff49f5921aa4271c33a0b333d47c1cca
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-04-28 11:43:59 -0600

    Añadir variables de max, min y step

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import {useState} from 'react'
-import Header from "./components/Header"
-function App() {
+import { useState } from 'react'
+import Header from './components/Header'
+function App () {
+  const [cantidad, setCantidad] = useState(10000)
+  const MIN = 0
+  const MAX = 20000
+  const STEP = 100
 
-  const [cantidad, setCantidad] = useState(10000);
-  
-  function handleChange(e) {
+  function handleChange (e) {
     setCantidad(parseInt(e.target.value))
   }
-// En el return no es recomendable mezclar la logica, 
-// esta reservada para la vista
+  // En el return no es recomendable mezclar la logica,
+  // esta reservada para la vista
   return (
-    
+
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
-      <input 
-        type="range" 
+      <input
+        type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange = {handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
       />
       <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-function Header() {
-  return(
+function Header () {
+  return (
     <h1 className="text-4xl font-extrabold text-gray-500 text-center">
         ¿Cuanto <span className="text-indigo-600">dinero</span> necesitas?
       </h1>
